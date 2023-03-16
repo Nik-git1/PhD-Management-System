12a4a1326ed02159722862476cd880c8c7db38ba
Atyant Sony <atyant20039@iiitd.ac.in>
2023-03-17 03:20:32 +0530

    Added a Basic NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <Header />
       <h1 className="text-7xl font-bold">PhD Management System</h1>
     </div>
   );
