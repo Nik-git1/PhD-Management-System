56321c964306092bddc01ecf1a13262056151b7f
Atyant Sony <atyant20039@iiitd.ac.in>
2023-03-17 05:38:01 +0530

    Added Icons to buttons and changed colors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Example() {
   return (
     <header className="bg-white">
       <nav
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex max-w-7xl items-center justify-between px-6 pb-6 pt-2 lg:px-8"
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,16 @@ module.exports = {
         sans: ['Inter var'],
       }
     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'pantoneblue': '#3fada8',
+      'pantonecoolgrey9': '#808080',
+      'pantonecoolgrey11': '#4d4d4d',
+      'pantonegrey4': '#666666',
+      'pantonegrey5': '#333333',
+    }
   },
   plugins: [],
 }
